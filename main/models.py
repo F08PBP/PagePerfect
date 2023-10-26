895b469a90ee9f9f9bb21da551f188e80f1a0883
HanifRifky <114400903+HanifRifky@users.noreply.github.com>
2023-10-26 20:14:58 +0700

    Update Data Dataset

--- main/models.py
+++ main/models.py
@@ -3,16 +3,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+USER_ROLES = (
+        ('member', 'Member'),
+        ('employee', 'Employee'),
+        ('writer', 'Writer'),
+    )
+
 class Author(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     revenue = models.BigIntegerField()
+    role = models.CharField(max_length=10, choices=USER_ROLES)
 
 class Member(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     money = models.BigIntegerField()
+    role = models.CharField(max_length=10, choices=USER_ROLES)
 
 class Employee(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=USER_ROLES)
 
 
 
