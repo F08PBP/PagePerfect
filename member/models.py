4fd0cc5367255afe52f40481cc16324e5da2d245
ternaksapi <yusufhaikaln7@gmail.com>
2023-10-29 15:44:39 +0700

    added cart button

--- member/models.py
+++ member/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from book.models import Book
 
 # Create your models here.
+class Keranjang(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    jumlah = models.IntegerField(default=0)
+    total_harga = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.user) + " " + str(self.book)
