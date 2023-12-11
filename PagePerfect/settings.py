f26e441ac95c010f3d2f9f46e1e8b9bfc21453bd
Arya Lesmana <arya.lesmana21@ui.ac.id>
2023-12-11 21:19:20 +0700

    delete import environ in settings.py

--- PagePerfect/settings.py
+++ PagePerfect/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ # Tambahkan kode berikut
 import os # Tambahkan kode berikut
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
