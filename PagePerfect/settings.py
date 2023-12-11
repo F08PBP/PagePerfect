8ac9c013337a17a0c864bc07395202efe42f048f
Arya Lesmana <arya.lesmana21@ui.ac.id>
2023-12-11 21:06:45 +0700

    rename Requirements.txt to requirements.txt

--- PagePerfect/settings.py
+++ PagePerfect/settings.py
@@ -17,7 +17,6 @@ import os # Tambahkan kode berikut
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env() # Tambahkan kode berikut
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,9 +24,6 @@ env = environ.Env() # Tambahkan kode berikut
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l=gvi!2s0hdp-m!j+c&grhel(p-)d=ks7)jr+*$k43c90a*i%#'
 
-# Automatically determine environment by detecting if DATABASE_URL variable.
-# DATABASE_URL is provided by Heroku if a database add-on is added (e.g. Heroku Postgres).
-PRODUCTION = env.bool('PRODUCTION', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ import os
 import sys
 import django
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PagePerfect.settings')
