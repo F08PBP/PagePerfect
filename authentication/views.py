06866af646d7857420ea8a71c7e34213a79ed334
RizvanuSN <rizvanusn@gmail.com>
2023-12-12 14:06:20 +0700

    benerin web

--- authentication/views.py
+++ authentication/views.py
@@ -3,33 +3,51 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from main.models import Author, Employee, Member
+
 @csrf_exempt
 def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
             auth_login(request, user)
-            # Status login sukses.
-            return JsonResponse({
-                "username": user.username,
-                "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
-            }, status=200)
+
+            # Menentukan role pengguna
+            role = None
+            if Member.objects.filter(user=user).exists():
+                role = 'Member'
+            elif Author.objects.filter(user=user).exists():
+                role = 'Writer'
+            elif Employee.objects.filter(user=user).exists():
+                role = 'Employee'
+
+            # Mengirimkan respons JSON ke Flutter
+            if role:
+                return JsonResponse({
+                    "username": user.username,
+                    "status": True,
+                    "role": role,
+                    "message": "Login sukses!"
+                }, status=200)
+            else:
+                return JsonResponse({
+                    "status": False,
+                    "message": "Login gagal, role pengguna tidak ditemukan."
+                }, status=401)
         else:
             return JsonResponse({
                 "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
+                "message": "Login gagal, periksa kembali username atau kata sandi."
             }, status=401)
-
     else:
         return JsonResponse({
             "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
-        }, status=401)
-    
+            "message": "Invalid request method."
+        }, status=400)
+
 
 @csrf_exempt
 def logout(request):
